bccefaced0f5c8d1586a54eaa6e87f64f1a08953
Varvara Sautkina <sautkinav@gmail.com>
2019-09-03 18:20:42 +0400

    Task 8

---
+++ FourthClassSeventhTask/FourthClassSeventhTask/EighthTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+
+namespace FourthClassEighthTask
+{
+    [TestFixture]
+    public class EighthTaskTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            //driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
+            driver = new EdgeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        [Test]
+        public void StickerByProduct()
+        {           
+            driver.Url = "http://localhost/litecart/";
+            var products = driver.FindElements(By.CssSelector(".product"));
+            foreach (var product in products)
+            {
+                var stickers = product.FindElements(By.CssSelector(".sticker"));
+                Assert.AreEqual(1, stickers.Count, "Houston, we have a problem with stickers");
+            }
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ FourthClassSeventhTask/FourthClassSeventhTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FourthClassSeventhTask")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("FourthClassSeventhTask")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("527b9543-ba3d-4dc2-8593-9694cc440e70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
