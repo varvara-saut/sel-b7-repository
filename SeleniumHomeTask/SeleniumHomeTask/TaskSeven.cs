32c7c851b4eb1cfe88bb55edfa505cdea5ed7ed8
Varvara Sautkina <sautkinav@gmail.com>
2019-09-04 18:21:05 +0400

    Task 7

---
+++ SeleniumHomeTask/SeleniumHomeTask/TaskSeven.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace SeleniumHomeTask
+{
+    [TestFixture]
+    public class TaskSevenTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
+            //driver = new EdgeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        }
+
+        [Test]
+        public void Menu()
+        {
+            //Стартовая страница
+            driver.Url = "http://localhost/litecart/admin/";
+            if (driver.FindElements(By.Name("login")).Count != 0)
+            { //Проверка для Edge, чтобы убедиться, что пользователь не был залогинен
+                //Авторизация в системе
+                driver.FindElement(By.Name("username")).SendKeys("admin");
+                driver.FindElement(By.Name("password")).SendKeys("admin");
+                driver.FindElement(By.Name("remember_me")).Click();
+                driver.FindElement(By.Name("login")).Click();
+            }
+            var menu = driver.FindElements(By.CssSelector("#box-apps-menu > li > a")); //выделение блока с меню
+            for (int i = 0; i < menu.Count; i++)
+            {
+                menu[i].Click();
+                Assert.AreNotEqual(0, driver.FindElements(By.CssSelector("h1")), "Проблемы со страницей");
+                var submenu = driver.FindElements(By.CssSelector("#box-apps-menu .docs a"));
+                if (submenu.Count > 0)
+                {
+                    for (int j = 0; j < submenu.Count; j++)
+                    {
+                        submenu[j].Click();
+                        Assert.AreNotEqual(0, driver.FindElements(By.CssSelector("h1")), "Проблемы со страницей");
+                        submenu = driver.FindElements(By.CssSelector("#box-apps-menu .docs a"));                       
+                    }
+                }
+                menu = driver.FindElements(By.CssSelector("#box-apps-menu > li > a"));
+            }
+
+            driver.FindElement(By.CssSelector("[title='Logout']")).Click();
+
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
